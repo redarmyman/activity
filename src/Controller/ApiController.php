1b52480f5b2f4284c6be1b3de3b1b2ed3a9a1b37
Piotr Redkowiak <p.redkowiak@gmail.com>
2023-03-08 08:50:15 +0000

    Lat & lon missing in request validation

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -11,6 +11,13 @@ class ApiController
     #[Route('/v1/isitraining', name: 'api_is_it_raining')]
     public function isItRaining(Request $request): JsonResponse
     {
+        if (null === $request->query->get('lat') || null === $request->query->get('lon')) {
+            return new JsonResponse(
+                ['error' => 'Missing lat or lon parameter'],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
         return new JsonResponse([
             'lat' => $request->query->get('lat'),
             'lon' => $request->query->get('lon')
--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -6,6 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
+    public function testIsItRainingMissingLatRequest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/isitraining?lon=51.110743');
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testIsItRainingMissingLonRequest(): void
+    {   
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/v1/isitraining?lat==51.110743');
+        
+        $this->assertResponseStatusCodeSame(400);
+    }
+
     public function testIsItRainingSuccessfulRequest(): void
     {
         $client = static::createClient();
