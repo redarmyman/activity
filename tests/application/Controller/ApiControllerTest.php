35ac870f72a96255fa3ee8f52b3b6d890536853d
Piotr Redkowiak <p.redkowiak@gmail.com>
2023-03-10 09:09:31 +0000

    Update ApiController tests

--- tests/application/Controller/ApiControllerTest.php
+++ tests/application/Controller/ApiControllerTest.php
@@ -13,15 +13,29 @@ class ApiControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/v1/isitraining?lon=51.110743');
 
         $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'Missing lat or lon parameter');
     }
 
     public function testIsItRainingMissingLonRequest(): void
     {   
         $client = static::createClient();
-        
+
         $crawler = $client->request('GET', '/v1/isitraining?lat==51.110743');
-        
+
         $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'Missing lat or lon parameter');
     }
 
     public function testIsItRainingWrongLongitudeRequest(): void
@@ -31,6 +45,13 @@ class ApiControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/v1/isitraining?lat=51.110743&lon=5000');
         
         $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+    
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'wrong longitude');
     }
 
     public function testIsItRainingWrongLatitudeRequest(): void
@@ -40,6 +61,13 @@ class ApiControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/v1/isitraining?lat=5000&lon=17.035002');
 
         $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'wrong latitude');
     }
 
     public function testIsItRainingSuccessfulRequest(): void
@@ -49,6 +77,13 @@ class ApiControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/v1/isitraining?lat=51.110743&lon=17.035002');
 
         $this->assertResponseIsSuccessful();
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 200);
+        $this->assertArrayHasKey('isItRaining', $response);
+        $this->assertIsBool($response['isItRaining']);
     }
 }
 
