97ef5ce5d12f582ccae37953fd74e41961afbd98
Piotr Redkowiak <p.redkowiak@gmail.com>
2023-03-11 13:34:21 +0000

    ApiController tests update

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Service\WeatherService;
 use App\Service\MessageGenerator;
 use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ServerException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,20 +13,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
+    public function __construct(private WeatherService $weather)
+    {}
+
     #[Route('/v1/isitraining', name: 'api_is_it_raining')]
-    public function isItRaining(WeatherService $weather, Request $request): JsonResponse
+    public function isItRaining(Request $request): JsonResponse
     {
-        if (null === $request->query->get('lat') || null === $request->query->get('lon')) {
+        return $this->isItRainingResponse($request->query->get('lat'), $request->query->get('lon'));
+    }
+
+    #[Route('/v1/whattodo', name: 'api_activity')]
+    public function activity(Request $request): JsonResponse
+    {
+        $isItRainingResponse = $this->isItRaining($request);
+
+        $isItRaining = json_decode($isItRainingResponse->getContent(), true);
+
+        if ($isItRaining['code'] > 399) {
+            return $isItRainingResponse;
+        }
+
+        dd($isItRaining);
+    }
+
+    private function isItRainingResponse(?string $lat, ?string $lon): JsonResponse
+    {
+        if (null === $lat || null === $lon) {
             return $this->getRainingResponse(JsonResponse::HTTP_BAD_REQUEST, 'Missing lat or lon parameter');
         }
 
         try {
-            $ifRaining = $weather->ifRaining($request->query->get('lat'), $request->query->get('lon'));
+            $ifRaining = $this->weather->ifRaining($lat, $lon);
         } catch(ClientException|ServerException $e) {
             return $this->getRainingResponse(
                 $e->getResponse()->getStatusCode(),
                 json_decode((string) $e->getResponse()->getBody(), true)['message']
             );
+        } catch (\Exception $e) {
+            return $this->getRainingResponse(JsonResponse::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
         }
 
         return $this->getRainingResponse(JsonResponse::HTTP_OK, '', $ifRaining);
--- tests/application/Controller/ApiControllerTest.php
+++ tests/application/Controller/ApiControllerTest.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
+    /* isitraing endpoint tests */
     public function testIsItRainingMissingLatRequest(): void
     {
         $client = static::createClient();
@@ -75,6 +76,87 @@ class ApiControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/v1/isitraining?lat=51.110743&lon=17.035002');
+
+        $this->assertResponseIsSuccessful();
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 200);
+        $this->assertArrayHasKey('isItRaining', $response);
+        $this->assertIsBool($response['isItRaining']);
+    }
+
+    /* whattodo endpoint tests */
+    public function testWhatToDoMissingLatRequest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/whattodo?lon=51.110743');
+
+        $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'Missing lat or lon parameter');
+    }
+
+    public function testWhatToDoMissingLonRequest(): void
+    {  
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/whattodo?lat==51.110743');
+
+        $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'Missing lat or lon parameter');
+    }
+
+    public function testWhatToDoWrongLongitudeRequest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/whattodo?lat=51.110743&lon=5000');
+
+        $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'wrong longitude');
+    }
+
+    public function testWhatToDoWrongLatitudeRequest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/whattodo?lat=5000&lon=17.035002');
+
+        $this->assertResponseStatusCodeSame(400);
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('code', $response);
+        $this->assertSame($response['code'], 400);
+        $this->assertArrayHasKey('message', $response);
+        $this->assertSame($response['message'], 'wrong latitude');
+    }
+
+    public function testActivitySuccessfulRequest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/v1/whattodo?lat=51.110743&lon=17.035002');
 
         $this->assertResponseIsSuccessful();
 
